f861e2e6e0a41ad266e131cdfd5a04aa1cec6775
Madina Beishenalieva <madinakg2003@gmail.com>
2024-03-26 21:28:10 +0600

    init commit

---
+++ src/main/java/com/example/onlinequizplatform/OnlineQuizPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinequizplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineQuizPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinequizplatform/OnlineQuizPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinequizplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
