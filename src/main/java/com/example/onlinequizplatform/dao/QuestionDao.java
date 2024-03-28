42a4a03bb896a0f2cabff4ff64de991542dc6f8e
Adil Rysbaev <adilrysbaev@gmail.com>
2024-03-28 15:53:31 +0600

    'Option Dao' classes 'Questions Dao' 'Quiz Dao' has been changed

--- src/main/java/com/example/onlinequizplatform/dao/QuestionDao.java
+++ src/main/java/com/example/onlinequizplatform/dao/QuestionDao.java
@@ -1,10 +1,7 @@
-package com.example.onlinequizplatform.dao;
-
+import com.example.onlinequizplatform.models.Option;
 import com.example.onlinequizplatform.models.Question;
-import com.example.onlinequizplatform.models.Quiz;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
@@ -15,4 +12,30 @@ import java.util.Objects;
 @Component
 @RequiredArgsConstructor
 public class QuestionDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public Long createQuestion(Question question) {
+        String sql = "INSERT INTO questions (questionText, quizId) VALUES (?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, question.getQuestionText());
+            ps.setLong(2, question.getQuizId());
+            return ps;
+        }, keyHolder);
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
+
+    public Long createOption(Option option) {
+        String sql = "INSERT INTO options (optionText, isCorrect, questionId) VALUES (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, option.getOptionText());
+            ps.setBoolean(2, option.getIsCorrect());
+            ps.setLong(3, option.getQuestionId());
+            return ps;
+        }, keyHolder);
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
 }
--- src/main/java/com/example/onlinequizplatform/dao/QuizDao.java
+++ src/main/java/com/example/onlinequizplatform/dao/QuizDao.java
@@ -1,5 +1,3 @@
-package com.example.onlinequizplatform.dao;
-
 import com.example.onlinequizplatform.models.Quiz;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
